b719386215f148ed14a0aa08e28f627f626b2278
Keegan Osler <keegan.osler@vergeag.com>
2022-03-20 17:18:18 -0600

    active session HTML scaffolding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Custom Interval Timer';
 
-  isActiveSession: boolean = false;
+  isActiveSession: boolean = true;
 }
