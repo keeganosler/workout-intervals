271fa5911e1e3b201b65b65f0f32b757af5a3e85
Keegan Osler <keegan.osler@vergeag.com>
2022-03-20 16:55:41 -0600

    basic form with defaults

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InactiveSessionComponent } from './components/inactive-session/inactive-session.component';
 import { SessionDetailsModalComponent } from './components/inactive-session/session-details-modal/session-details-modal.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     InactiveSessionComponent,
-    SessionDetailsModalComponent
+    SessionDetailsModalComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/inactive-session/session-details-modal/session-details-modal.component.ts
+++ src/app/components/inactive-session/session-details-modal/session-details-modal.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-session-details-modal',
   templateUrl: './session-details-modal.component.html',
-  styleUrls: ['./session-details-modal.component.scss']
+  styleUrls: ['./session-details-modal.component.scss'],
 })
 export class SessionDetailsModalComponent implements OnInit {
+  form = this.formBuilder.group({
+    warmupTime: this.formBuilder.group({
+      minutes: [5],
+      seconds: [0],
+    }),
+    numberOfIntervals: [6],
+    intervalTime: this.formBuilder.group({
+      minutes: [1],
+      seconds: [30],
+    }),
+    cooldownTime: this.formBuilder.group({
+      minutes: [7],
+      seconds: [0],
+    }),
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit() {
+    console.log(this.form.value);
   }
-
 }
