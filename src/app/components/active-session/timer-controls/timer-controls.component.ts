5eca3872ed38eb7a756e20ea9ebdbf1f06194c13
Keegan Osler <keegan.osler@vergeag.com>
2022-03-20 17:42:58 -0600

    styling active session

--- src/app/components/active-session/timer-controls/timer-controls.component.ts
+++ src/app/components/active-session/timer-controls/timer-controls.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { faPause } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-timer-controls',
   templateUrl: './timer-controls.component.html',
-  styleUrls: ['./timer-controls.component.scss']
+  styleUrls: ['./timer-controls.component.scss'],
 })
 export class TimerControlsComponent implements OnInit {
+  faPause = faPause;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
